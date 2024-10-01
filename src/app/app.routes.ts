5bf55646549b542737bde8e4235f5632e8d35d2c
rawanfouda213 <rawanfouda213@gmail.com>
2024-10-01 16:51:20 +0300

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './Components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'footer' , component: FooterComponent},
+    { path: '', redirectTo: '/footer', pathMatch: 'full' }
+];
