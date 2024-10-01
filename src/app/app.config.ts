be2e2823dcffd9e9818f0b890f7a744fa01e8435
rawanfouda213 <rawanfouda213@gmail.com>
2024-10-01 17:55:18 +0300

    task done :)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+<<<<<<< HEAD
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+=======
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+>>>>>>> Rawan-01-10-2024-Sections-Footer
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+import { provideRouter, RouterModule, Routes } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { TrainingComponent } from './components/training/training.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/news', pathMatch: 'full' },
+  { path: 'news', component: NewsComponent },
+  { path: 'training', component: TrainingComponent }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+});
+=======
 import { Routes } from '@angular/router';
 import { FooterComponent } from './Components/footer/footer.component';
 
@@ -5,3 +24,4 @@ export const routes: Routes = [
     {path:'footer' , component: FooterComponent},
     { path: '', redirectTo: '/footer', pathMatch: 'full' }
 ];
+>>>>>>> Rawan-01-10-2024-Sections-Footer
